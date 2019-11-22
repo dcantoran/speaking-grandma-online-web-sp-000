2dc831ddd2d9c37f18cb477b80f4624a632b3238
David Cantoran <davidcantoran36@gmail.com>
2019-11-22 17:27:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(statement)
   if statement == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif statement == statement.upcase
+  elsif statement.upcase
     "NO, NOT SINCE 1938!"
-  elsif !statement.upcase 
+  elsif statement != statement.upcase 
     "HUH?! SPEAK UP, SONNY!"
   end 
 end 
